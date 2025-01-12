12515e69c614241b45696d7925cfd275f77efe0c
BlossomBuild <blossombuildLLC@outlook.com>
2025-01-11 21:08:19 -0700

    Initial Commit

---
+++ BlossomMovie/BlossomMovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlossomMovieApp.swift
+//  BlossomMovie
+//
+//  Created by Carlos Valentin on 11/7/24.
+//
+
+import SwiftUI
+
+@main
+struct BlossomMovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
