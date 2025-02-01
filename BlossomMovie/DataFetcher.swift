654a68d052ee3069959efb7a453c5d39bb308a4d
BlossomBuild <blossombuildLLC@outlook.com>
2025-01-31 23:20:31 -0700

    section 2-4

---
+++ BlossomMovie/DataFetcher.swift
@@ -0,0 +1,27 @@
+//
+//  DataFetcher.swift
+//  BlossomMovie
+//
+//  Created by Carlos Valentin on 1/30/25.
+//
+
+import Foundation
+
+let tmdbBaseURL = APIConfig.shared?.tmdbBaseURL
+let tmdbAPIKey = APIConfig.shared?.tmdbAPIKey
+
+//https://api.themoviedb.org/3/trending/movie/day?api_key=YOUR_API_KEY
+func fetchTitles(for media:String) async throws -> [Title] {
+    guard let baseURL = tmdbBaseURL else {
+        throw NetworkError.missingConfig
+    }
+    guard let apiKey = tmdbAPIKey else {
+        throw NetworkError.missingConfig
+    }
+    
+    let fetchTitlesURL = URL(string: baseURL)?
+        .appending(path: "3/trending\(media)/day")
+        .appending(queryItems: [
+            URLQueryItem(name: "api_key", value: apiKey)
+        ])
+}
