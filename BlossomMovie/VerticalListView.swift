08d654e7669c961cb76ed03b3b98264bc0000899
BlossomBuild <blossombuildLLC@outlook.com>
2025-06-16 12:55:42 -0700

    Added NavigationLinks to the vertical list

--- BlossomMovie/VerticalListView.swift
+++ BlossomMovie/VerticalListView.swift
@@ -5,6 +5,9 @@
 //  Created by Carlos Valentin on 5/28/25.
 //
 
+
+
+
 import SwiftUI
 
 struct VerticalListView: View {
@@ -12,22 +15,28 @@ struct VerticalListView: View {
     
     var body: some View {
         List(titles) { title in
-            AsyncImage(url: URL(string: title.posterPath ?? "")) { image in
-                HStack {
-                    image
-                        .resizable()
-                        .scaledToFit()
-                        .clipShape(.rect(cornerRadius: 10))
-                        .padding(5)
-                    
-                    Text((title.name ?? title.title) ?? "")
-                        .font(.system(size: 14))
-                        .bold()
+            NavigationLink {
+                TitleDetailView(title: title)
+            } label: {
+                AsyncImage(url: URL(string: title.posterPath ?? "")) { image in
+                    HStack {
+                        image
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(.rect(cornerRadius: 10))
+                            .padding(5)
+                        
+                        Text((title.name ?? title.title) ?? "")
+                            .font(.system(size: 14))
+                            .bold()
+                    }
+                } placeholder: {
+                    ProgressView()
                 }
-            } placeholder: {
-                ProgressView()
+                .frame(height: 150)
             }
-            .frame(height: 150)
+            
+            
         }
     }
 }
